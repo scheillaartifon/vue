1ea97989f10ac0ff627ac00c41fbe9e76dc32764
Scheilla Artifon <scheillaartifon9805@hotmail.com>
2025-03-01 02:10:15 -0300

    Organizando estrutura e criando componentes por aulas

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <TheHeader v-show="showHeader" />
+
+  <AulaQuatro v-if="aula4"/>
+
+  <img
+    alt="Vue logo" 
+    src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import TheHeader from './components/TheHeader'
+import AulaQuatro from './components/AulaQuatro';
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    TheHeader,
+    AulaQuatro
+  },
+  data() {
+    return {
+      showHeader: true,
+      aula4: false
+    }
+  } 
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/AulaQuatro.vue
@@ -0,0 +1,28 @@
+<template>
+    <h5 class="mb-0">AULA 4 - Diretivas</h5>
+    <div v-show="showName">
+        Nome: {{ firstName }} <br>
+        Sobrenome: {{ lastName }}
+    </div>
+
+    <div v-if="accessLevel === 'admin'">Usuário Admin</div>
+    <div v-else-if="accessLevel === 'marketing'">Usuário Marketing</div>
+    <div v-else>Usuário Normal</div>
+</template>
+
+<script>
+
+export default {
+    data() {
+        return {
+            firstName: 'Jhon',
+            lastName: 'Snow',
+            showName: true,
+            accessLevel: 'admin'
+        }
+    }
+}
+
+</script>
+
+<style></style>
