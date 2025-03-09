e334fafb5844a67b1451cf7dfd94e8b52356e203
Scheilla Artifon <scheillaartifon9805@hotmail.com>
2025-03-09 00:17:08 -0300

    AULA 10 - Propriedade computada (computed)

---
+++ src/components/LessonNine.vue
@@ -0,0 +1,46 @@
+<!-- Site que peguei o JSON -->
+<!-- https://jsonplaceholder.typicode.com -->
+
+<template>
+    <h5>AULA 9 - Eventos</h5>
+    
+    <!-- Executa só uma vez @click.once="onClick" -->
+    <button @click="onClick">Click Button</button><br>
+
+    <div @mouseover="onMouseOver" @mouseout="onMouseOut">Mouse Over</div>
+
+    <!-- Não direcionar para o google @submit.prevent -->
+    <form action="https://google.com" @submit="onSubmit">
+        <!-- Só envia o que foi digitado no teclado quando apertado enter @keyup.enter -->
+        <input type="text" @keyup="onKeyUp">
+        <button type="submit">Enviar</button>
+    </form>
+</template>
+
+<script>
+export default{
+    data() {
+        return{
+        }
+    },
+    methods: {
+        onClick($evt) {
+            console.log('Click', $evt)
+        },
+        onMouseOver($evt){
+            console.log("Mouse Over", $evt)
+        },
+        onMouseOut($evt){
+            console.log("Mouse Out", $evt)
+        },
+        onSubmit($evt){
+            console.log("submit", $evt)
+        },
+        onKeyUp($evt){
+            console.log("onKeyUp", $evt)
+        }
+    }
+}
+</script>
+
+<style></style>
---
+++ src/components/LessonTen.vue
@@ -0,0 +1,88 @@
+<!-- Site que peguei o JSON -->
+<!-- https://jsonplaceholder.typicode.com -->
+
+<template>
+    <h5>AULA 10 - Propriedade computada (computed)</h5>
+    
+    <div>{{ fullName }}</div> <hr>
+
+    <h5>Todos como false</h5>
+    <div v-for="todo in todosFalse" :key="todo.id">
+        {{ todo.title }}
+    </div>
+
+    <hr>
+
+    <h5>Todos como true</h5>
+    <div v-for="todo in todosTrue" :key="todo.id">
+        {{ todo.title }}
+    </div>
+
+    <hr>
+
+    <h5>Todos os itens da lista</h5>
+    <div v-for="todo in todos" :key="todo.id">
+        <input v-model="todo.completed" type="checkbox">
+        {{ todo.title }}
+    </div>
+</template>
+
+<script>
+export default{
+    data() {
+        return{
+            user: {
+                first_name: 'Jhon',
+                last_name: 'Snow'
+            },
+            todos: [
+                {
+                    "userId": 1,
+                    "id": 1,
+                    "title": "delectus aut autem",
+                    "completed": false
+                },
+                {
+                    "userId": 1,
+                    "id": 2,
+                    "title": "quis ut nam facilis et officia qui",
+                    "completed": false
+                },
+                {
+                    "userId": 1,
+                    "id": 3,
+                    "title": "fugiat veniam minus",
+                    "completed": false
+                },
+                {
+                    "userId": 1,
+                    "id": 4,
+                    "title": "et porro tempora",
+                    "completed": true
+                },
+                {
+                    "userId": 1,
+                    "id": 5,
+                    "title": "laboriosam mollitia et enim quasi adipisci quia provident illum",
+                    "completed": false
+                }
+            ]
+        }
+    },
+    computed: {
+        fullName(){
+            return `${this.user.first_name} ${this.user.last_name}`
+        },
+        todosFalse(){
+            // Retorna as todos nao completadas
+            return this.todos.filter(todo => !todo.completed)
+        },
+        todosTrue(){
+            return this.todos.filter(todo => todo.completed)
+        }
+    },
+    methods: { }
+}
+</script>
+
+<style></style>
